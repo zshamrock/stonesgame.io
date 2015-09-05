9abf3633c149198dd1167f85d87db8f62f073f4d
Aliaksandr Kazlou <aliaksandr.kazlou@gmail.com>
2015-09-05 17:03:35 +0200

    Second iteration. Player class introduced.

--- src/java/com/bol/game/Game.java
+++ src/java/com/bol/game/Game.java
@@ -1,6 +1,5 @@
 package com.bol.game;
 public final class Game {
-    public static final int TOTAL_PITS = 7;
 
     public enum Status {
         GAME_OVER,
@@ -8,16 +7,8 @@ public final class Game {
         SAME_PLAYER
     }
 
-    public int[][] turn(final int[] player1, final int pit, final int[] player2) {
+    public void turn(final Player p1, final int pit, final Player p2) {
         // assume all arguments are correct, no validation, specially for pit
-        final int stones = player1[pit];
-        final int[][] players = new int[2][TOTAL_PITS];
-        players[0][pit] = 0;
-        for (int i = 1; i <= stones; i++) {
-            final int position = (pit + i) % TOTAL_PITS;
-            players[0][(position)] = player1[(position)] + 1;
-        }
-        System.arraycopy(player2, 0, players[1], 0, TOTAL_PITS);
-        return players;
+        p1.pick(pit);
     }
 }
--- test/groovy/com/bol/game/GameSpec.groovy
+++ test/groovy/com/bol/game/GameSpec.groovy
@@ -13,19 +13,25 @@ class GameSpec extends Specification {
 
     def "make a turn in the beginning of a game"() {
         when:
-        def players = game.turn(p1before as int[], pit, p2before as int[])
+        def p1 = new Player(pits1 as int[])
+        def p2 = new Player(pits2 as int[])
+        game.turn(p1, pit, p2)
 
         then:
-        players[0] == p1after as int[]
-        players[1] == p2before as int[]
+        p1.pits == outcome as int[]
+        p2.pits == pits2 as int[]
 
         where:
-        p1before              | pit | p2before              || p1after
+        pits1                 | pit | pits2                 || outcome
         [6, 6, 6, 6, 6, 6, 0] | 0   | [6, 6, 6, 6, 6, 6, 0] || [0, 7, 7, 7, 7, 7, 1]
         [6, 6, 6, 6, 6, 6, 0] | 1   | [6, 6, 6, 6, 6, 6, 0] || [7, 0, 7, 7, 7, 7, 1]
         [6, 6, 6, 6, 6, 6, 0] | 2   | [6, 6, 6, 6, 6, 6, 0] || [7, 7, 0, 7, 7, 7, 1]
         [6, 6, 6, 6, 6, 6, 0] | 3   | [6, 6, 6, 6, 6, 6, 0] || [7, 7, 7, 0, 7, 7, 1]
         [6, 6, 6, 6, 6, 6, 0] | 4   | [6, 6, 6, 6, 6, 6, 0] || [7, 7, 7, 7, 0, 7, 1]
         [6, 6, 6, 6, 6, 6, 0] | 5   | [6, 6, 6, 6, 6, 6, 0] || [7, 7, 7, 7, 7, 0, 1]
+    }
+
+    def "result of a turn"() {
+
     }
 }
