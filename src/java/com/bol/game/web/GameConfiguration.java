fc5cd0ca8f4559792fe86144e32a3b1c39698d0f
Aliaksandr Kazlou <aliaksandr.kazlou@gmail.com>
2015-10-06 00:31:55 +0200

    Configure players queue capacity in the game.yaml config file.

--- src/java/com/bol/game/web/GameConfiguration.java
+++ src/java/com/bol/game/web/GameConfiguration.java
@@ -1,6 +1,35 @@
 package com.bol.game.web;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.google.common.base.MoreObjects;
 import io.dropwizard.Configuration;
 
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
+
 public class GameConfiguration extends Configuration {
+    public static final String QUEUE_CAPACITY = "QUEUE_CAPACITY";
+
+    protected static final int MAX_QUEUE_CAPACITY = 100;
+
+    @Min(0)
+    @Max(MAX_QUEUE_CAPACITY)
+    private int queueCapacity;
+
+    public int getQueueCapacity() {
+        final int capacity = Math.min(
+                // protect max queue capacity
+                MAX_QUEUE_CAPACITY,
+                Integer.parseInt(
+                    // look for the queue capacity in the following order: env property -> yaml property
+                    MoreObjects.firstNonNull(
+                            System.getenv(QUEUE_CAPACITY),
+                            Integer.toString(this.queueCapacity))));
+        return capacity >= 0 ? capacity : MAX_QUEUE_CAPACITY;
+    }
+
+    @JsonProperty
+    public void setQueueCapacity(final int queueCapacity) {
+        this.queueCapacity = queueCapacity;
+    }
 }
---
+++ test/groovy/com/bol/game/web/GameConfigurationSpec.groovy
@@ -0,0 +1,24 @@
+package com.bol.game.web
+
+import spock.lang.Specification
+
+
+class GameConfigurationSpec extends Specification {
+    def "get queue capacity"() {
+        given:
+        def config = new GameConfiguration()
+
+        when:
+        config.setQueueCapacity(inCapacity)
+
+        then:
+        config.queueCapacity == outCapacity
+
+        where:
+        inCapacity                               || outCapacity
+        -1                                       || GameConfiguration.MAX_QUEUE_CAPACITY
+        0                                        || 0
+        10                                       || 10
+        GameConfiguration.MAX_QUEUE_CAPACITY + 1 || GameConfiguration.MAX_QUEUE_CAPACITY
+    }
+}
